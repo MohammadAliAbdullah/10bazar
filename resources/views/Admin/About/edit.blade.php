59dc869b7f060bf1e67ff7d340bded038958fe19
mohammadaliabdullah <maabdullah511@gmail.com>
2025-07-03 14:12:23 +0600

    about breadcrumb

--- resources/views/Admin/About/edit.blade.php
+++ resources/views/Admin/About/edit.blade.php
@@ -1,8 +1,14 @@
 @extends('Admin.layouts.master')
 
 @section('content')
+    @include('Admin.include.breadcrumb', [
+        'page' => __('Edit About Us'),
+        'parent' => __('Home'),
+        'child' => __('About'),
+        'route' => '',
+    ])
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    {{-- <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
@@ -16,7 +22,7 @@
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
-    </div>
+    </div> --}}
     <!-- /.content-header -->
 
     <!-- Main content -->
--- resources/views/Admin/About/index.blade.php
+++ resources/views/Admin/About/index.blade.php
@@ -1,13 +1,20 @@
 @extends('Admin.layouts.master')
 
 @section('content')
+    @include('Admin.include.breadcrumb', [
+        'page' => __('About Us'),
+        'parent' => __('Home'),
+        'child' => __('About'),
+        'route' => '',
+    ])
+
     <!-- Content Header (Page header) -->
-    <div class="content-header">
+    {{-- <div class="content-header">
         <div class="container-fluid">
             <div class="row mb-2">
                 <div class="col-sm-6">
                     <h1 class="m-0 text-dark">About Us
-{{--                        <a href="{{ route('madmin.categories.create') }}" class="btn btn-primary">Category Add</a>--}}
+                        <!-- <a href="{{ route('madmin.categories.create') }}" class="btn btn-primary">Category Add</a> -->
                     </h1>
                 </div><!-- /.col -->
                 <div class="col-sm-6">
@@ -18,7 +25,7 @@
                 </div><!-- /.col -->
             </div><!-- /.row -->
         </div><!-- /.container-fluid -->
-    </div>
+    </div> --}}
     <!-- /.content-header -->
 
     <!-- Main content -->
@@ -28,46 +35,47 @@
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                    {{--                        <div class="card-header">--}}
-                    {{--                            <h3 class="card-title">Condensed Full Width Table</h3>--}}
-                    {{--                        </div>--}}
-                    <!-- /.card-header -->
+                        {{--                        <div class="card-header"> --}}
+                        {{--                            <h3 class="card-title">Condensed Full Width Table</h3> --}}
+                        {{--                        </div> --}}
+                        <!-- /.card-header -->
 
                         @include('Admin.include.message')
                         <div class="card-body p-0">
                             <table class="table table-bordered">
                                 <tbody>
-                                <tr>
-                                    <th>about_title</th>
-                                    <th>content</th>
-                                    <th>mission</th>
-                                    <th>vision</th>
-                                    <th>establistmet</th>
-                                    <th>Date</th>
-                                    <th>Action</th>
-                                </tr>
-                                @foreach($categories as $value)
                                     <tr>
-                                        <td>{{ $value->about_title }}</td>
-                                        <td>
-                                            {{ $value->content }}
-                                        </td>
-                                        <td>{{ $value->mission }}</td>
-                                        <td>{{ $value->vision }}</td>
-                                        <td>{{ $value->establistmet }}</td>
-                                        <td>{{ $value->created_at->diffForHumans() }} </td>
-
-                                        <td>
-                                            <div class="row">
-                                                    <a href="{{route('madmin.aboutadmin.edit',$value->id)}}" class="btn btn-success m-1"><i class="fa fa-pen"></i> </a>
-{{--                                                    {!! Form::open(['method'=>'DELETE','route'=>['madmin.categories.destroy',$value->id]]) !!}--}}
-{{--                                                    <button type="submit" value="Delete" class="btn btn-danger m-1" onclick="return confirm('Do you want to Delete, Delete with product')">X</button>--}}
-{{--                                                    {!! Form::close() !!}--}}
-
-                                            </div>
-                                        </td>
+                                        <th>about_title</th>
+                                        <th>content</th>
+                                        <th>mission</th>
+                                        <th>vision</th>
+                                        <th>establistmet</th>
+                                        <th>Date</th>
+                                        <th>Action</th>
                                     </tr>
-                                @endforeach
+                                    @foreach ($categories as $value)
+                                        <tr>
+                                            <td>{{ $value->about_title }}</td>
+                                            <td>
+                                                {{ $value->content }}
+                                            </td>
+                                            <td>{{ $value->mission }}</td>
+                                            <td>{{ $value->vision }}</td>
+                                            <td>{{ $value->establistmet }}</td>
+                                            <td>{{ $value->created_at->diffForHumans() }} </td>
+
+                                            <td>
+                                                <div class="row">
+                                                    <a href="{{ route('madmin.aboutadmin.edit', $value->id) }}"
+                                                        class="btn btn-success m-1"><i class="fa fa-pen"></i> </a>
+                                                    {{--                                                    {!! Form::open(['method'=>'DELETE','route'=>['madmin.categories.destroy',$value->id]]) !!} --}}
+                                                    {{--                                                    <button type="submit" value="Delete" class="btn btn-danger m-1" onclick="return confirm('Do you want to Delete, Delete with product')">X</button> --}}
+                                                    {{--                                                    {!! Form::close() !!} --}}
+
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
 
                                 </tbody>
                             </table>
