3637bf871ac74662c4524d9f1dd0b76fcfa76472
mohammadaliabdullah <maabdullah511@gmail.com>
2025-07-03 10:46:53 +0600

    dashboard breadcrumb

--- resources/views/Admin/deshboard.blade.php
+++ resources/views/Admin/deshboard.blade.php
@@ -1,23 +1,13 @@
 @extends('Admin.layouts.master')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0">Dashboard</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Home</a></li>
-                        <li class="breadcrumb-item active">Dashboard</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
-
+    @include('Admin.components.breadcrumb', [
+        'page' => __('Dashboard'),
+        'parent' => __('Home'),
+        'child' => __('Dashboard'),
+        'route' => route('madmin.dashboard')
+    ])
+    
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
