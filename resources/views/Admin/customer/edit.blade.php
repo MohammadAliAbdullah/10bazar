0410f6465d4da3e3c486a7f9f7b807728a4cdd0d
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 01:44:05 +0600

    customer design

--- resources/views/Admin/customer/edit.blade.php
+++ resources/views/Admin/customer/edit.blade.php
@@ -5,99 +5,85 @@
         'page' => __('Edit Customer'),
         'parent' => __('Home'),
         'child' => __('Customer'),
+        'button' => __('Customer List'),
+        'button_icon' => 'lni-text-align-justify',
         'route' => route('madmin.customeradmin.index'),
     ])
-    <!-- Content Header (Page header) -->
-    {{-- <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Edit Customer</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('madmin.customeradmin.index') }}">Customer</a></li>
-                        <li class="breadcrumb-item active">Edit Customer</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div> --}}
-    <!-- /.content-header -->
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
-            <div class="row">
-                <div class="col-md-6">
-                    <div class="card card-info">
-                    {{--                <div class="card-header">--}}
-                    {{--                    <h3 class="card-title">Horizontal Form</h3>--}}
-                    {{--                </div>--}}
-                    <!-- /.card-header -->
-                    @include('Admin.include.message')
-                    <!-- form start -->
-                        {!! Form::model($customer, ['method'=>'PATCH','route'=>['madmin.customeradmin.update', $customer],'class'=>'form-horizontal', 'files'=>true]) !!}
-                        <div class="card-body">
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @include('Admin.include.message')
 
+                <div class="card">
+                    <div class="card-body">
+                        {!! Form::model($customer, [
+                            'method' => 'PATCH',
+                            'route' => ['madmin.customeradmin.update', $customer],
+                            'class' => 'form-horizontal',
+                            'files' => true
+                        ]) !!}
 
-                            <div class="form-group row">
-                                {!! Form::label('name', 'Name', ['class' => 'col-sm-4 col-form-label']) !!}
-                                <div class="col-sm-8">
-                                    {!! Form::text('name', null, ['class'=>'form-control','id'=>'receiver','required']) !!}
-                                </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                {!! Form::label('name', 'Name') !!}
+                                {!! Form::text('name', null, [
+                                    'class' => 'form-control',
+                                    'placeholder' => 'Enter name',
+                                    'required'
+                                ]) !!}
                             </div>
 
-                            <div class="form-group row">
-                                {!! Form::label('name', 'Phone', ['class' => 'col-sm-4 col-form-label']) !!}
-                                <div class="col-sm-8">
-                                    {!! Form::number('phone', null, ['class'=>'form-control','id'=>'receiver','required']) !!}
-                                </div>
+                            <div class="form-group col-md-6">
+                                {!! Form::label('phone', 'Phone') !!}
+                                {!! Form::number('phone', null, [
+                                    'class' => 'form-control',
+                                    'placeholder' => 'Enter phone number',
+                                    'required'
+                                ]) !!}
                             </div>
-
-
-                            <div class="form-group row">
-                                {!! Form::label('name', 'Email', ['class' => 'col-sm-4 col-form-label']) !!}
-                                <div class="col-sm-8">
-                                    {!! Form::email('email', null, ['class'=>'form-control','id'=>'receiver']) !!}
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                {!! Form::label('name', 'Address', ['class' => 'col-sm-4 col-form-label']) !!}
-                                <div class="col-sm-8">
-                                    {!! Form::textarea('address', null, ['class'=>'form-control','id'=>'receiver','rows'=>'3','required']) !!}
-                                </div>
-                            </div>
-
-                            <div class="form-group row">
-                                {!! Form::label('name', 'Password', ['class' => 'col-sm-4 col-form-label']) !!}
-                                <div class="col-sm-8">
-                                    {!! Form::text('password', null, ['class'=>'form-control','id'=>'exampleInputPassword1','required']) !!}
-                                </div>
-                            </div>
-
-
                         </div>
-                        <!-- /.card-body -->
-                        <div class="card-footer">
 
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                {!! Form::label('email', 'Email') !!}
+                                {!! Form::email('email', null, [
+                                    'class' => 'form-control',
+                                    'placeholder' => 'Enter email address'
+                                ]) !!}
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                {!! Form::label('password', 'Password') !!}
+                                {!! Form::text('password', null, [
+                                    'class' => 'form-control',
+                                    'placeholder' => 'Enter new password',
+                                    'required'
+                                ]) !!}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-12">
+                                {!! Form::label('address', 'Address') !!}
+                                {!! Form::textarea('address', null, [
+                                    'class' => 'form-control',
+                                    'rows' => 3,
+                                    'placeholder' => 'Enter address',
+                                    'required'
+                                ]) !!}
+                            </div>
+                        </div>
+
+                        <div class="mt-3">
                             <button type="submit" class="btn btn-info">Update</button>
-                            <button  onclick="window.history.back()" class="btn btn-default float-right">Cancel</button>
+                            <button onclick="window.history.back()" class="btn btn-default">Cancel</button>
                         </div>
-                        <!-- /.card-footer -->
-                        {!! Form::close() !!}
 
+                        {!! Form::close() !!}
                     </div>
                 </div>
-
             </div>
-            <!-- /.row (main row) -->
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
-@endsection
-@section('script')
-
+        </div>
+    </div>
 @endsection
--- resources/views/Admin/customer/index.blade.php
+++ resources/views/Admin/customer/index.blade.php
@@ -5,98 +5,82 @@
         'page' => __('Customer'),
         'parent' => __('Home'),
         'child' => __('Customer'),
+        'button' => __('Add Customer'),
+        'button_icon' => 'lni-plus',
         'route' => route('madmin.customeradmin.create'),
     ])
-    <!-- Content Header (Page header) -->
-    {{-- <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Customer
-                        <a href="{{ route('madmin.customeradmin.create') }}" class="btn btn-primary">Customer Add</a>
-                    </h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Customer</a></li>
-                        <li class="breadcrumb-item active">Customer add</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div> --}}
-    <!-- /.content-header -->
 
-    <!-- Main content -->
-    <section class="content">
-        <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
-            <div class="row">
-                <div class="col-md-12">
-                    <div class="card">
-                    {{--                        <div class="card-header">--}}
-                    {{--                            <h3 class="card-title">Condensed Full Width Table</h3>--}}
-                    {{--                        </div>--}}
-                    <!-- /.card-header -->
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @include('Admin.include.message')
 
-                        @include('Admin.include.message')
-                        <div class="card-body p-0">
-                            <table class="table table-bordered">
-                                <tbody>
-                                <tr>
-                                    <th>SI</th>
-                                    <th>Name</th>
-                                    <th>Photo</th>
-                                    <th>Contact Info</th>
-                                    <th>Address</th>
-                                    <th>Date</th>
-                                    <th>Action</th>
-                                </tr>
-                                @foreach($customers as $value)
-                                    <tr>
-                                        <td>{{ $value->id }}</td>
-                                        <td>{{ $value->name }}</td>
-                                        <td>
-                                            <img src="{{ $value->images }}" class="img-thumbnail" alt="">
-                                        </td>
-                                        <td>
-                                            <b>Phone: </b>{{ $value->phone }} <br>
-                                            <b>Email: </b>{{ $value->email }} <br>
-                                        </td>
-                                        <td>
-                                           {{ $value->address }} <br>
-{{--                                            <b>Email: </b>{{ $value->email }} <br>--}}
-                                        </td>
-
-                                        <td>{{ $value->created_at->diffForHumans() }} </td>
-
-                                        <td>
-                                            <div class="row">
-                                                    <a href="{{route('madmin.customeradmin.edit',$value->id)}}" class="btn btn-success m-1"><i class="lni-pencil-alt"></i> </a>
-                                                    {!! Form::open(['method'=>'DELETE','route'=>['madmin.customeradmin.destroy',$value->id]]) !!}
-                                                    <button type="submit" value="Delete" class="btn btn-danger m-1" onclick="return confirm('Do you want to Delete')"><i class="lni-trash"></i></button>
-                                                    {!! Form::close() !!}
-
-                                            </div>
-                                        </td>
+                <div class="card">
+                    <div class="card-body p-0">
+                        <div class="table-responsive">
+                            <table class="table table-bordered mb-0">
+                                <thead class="custom-thead">
+                                    <tr class="">
+                                        <th>SI</th>
+                                        <th>Name</th>
+                                        <th>Photo</th>
+                                        <th>Contact Info</th>
+                                        <th>Address</th>
+                                        <th>Date</th>
+                                        <th>Action</th>
                                     </tr>
-                                @endforeach
+                                </thead>
+                                <tbody>
+                                    @foreach ($customers as $value)
+                                        <tr>
+                                            <td class="text-center">{{ $loop->iteration }}</td>
+                                            <td>{{ $value->name }}</td>
+                                            <td>
+                                                <img src="{{ $value->images }}" class="img-thumbnail"
+                                                    style="width: 60px; height: 60px;" alt="Customer Image">
+                                            </td>
+                                            <td>
+                                                <strong>Phone:</strong> {{ $value->phone }}<br>
+                                                <strong>Email:</strong> {{ $value->email }}
+                                            </td>
+                                            <td>{{ $value->address }}</td>
+                                            <td>{{ $value->created_at->diffForHumans() }}</td>
+                                            <td>
+                                                <div class="d-flex flex-wrap justify-content-center">
+                                                    <a href="{{ route('madmin.customeradmin.edit', $value->id) }}"
+                                                        class="btn btn-sm btn-success m-1">
+                                                        <i class="lni-pencil-alt"></i>
+                                                    </a>
+                                                    {!! Form::open(['method' => 'DELETE', 'route' => ['madmin.customeradmin.destroy', $value->id]]) !!}
+                                                    <button type="submit" class="btn btn-sm btn-danger m-1"
+                                                        onclick="return confirm('Do you want to delete?')">
+                                                        <i class="lni-trash"></i>
+                                                    </button>
+                                                    {!! Form::close() !!}
+                                                </div>
+                                            </td>
+                                        </tr>
+                                    @endforeach
 
+                                    @if ($customers->isEmpty())
+                                        <tr>
+                                            <td colspan="7" class="text-center text-muted">No customers found.</td>
+                                        </tr>
+                                    @endif
                                 </tbody>
                             </table>
-
                         </div>
-                        <!-- /.card-body -->
                     </div>
-                    <div class="row">
-                        <div class="col-md-6 offset-md-2 m-0 float-right">
-                            {{ $customers->render() }}
+                    <!-- /.card-body -->
+
+                    <div class="card-footer">
+                        <div class="d-flex justify-content-end">
+                            {{ $customers->links() }}
                         </div>
                     </div>
                 </div>
+
             </div>
-            <!-- /.row (main row) -->
-        </div><!-- /.container-fluid -->
-    </section>
-    <!-- /.content -->
+        </div>
+    </div>
 @endsection
