0d76e6d4d9cafe4cf3d2e24190508e0d19758a2d
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 01:44:45 +0600

    sms log design

--- resources/views/Admin/Marketing/smslog/index.blade.php
+++ resources/views/Admin/Marketing/smslog/index.blade.php
@@ -1,148 +1,100 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">SMS Log Add
-
-                    </h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">SMS Log</a></li>
-                        <li class="breadcrumb-item active">SMS Log add</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    @include('Admin.include.breadcrumb', [
+        'page' => __('SMS Log Add'),
+        'parent' => __('SMS Log'),
+        'child' => __('Add'),
+        'button' => __('Back to List'),
+        'button_icon' => 'lni lni-list',
+        'route' => '#',  // adjust this route as needed  route('madmin.smslog.index')
+    ])
 
     <!-- Main content -->
     <section class="content">
         <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
+            <!-- Log table -->
             <div class="row">
-                <div class="col-md-12">
+                <div class="col-12">
                     <div class="card">
-{{--                    <div class="card-header">--}}
-{{--                        {{ Form::open(['route' => 'datacollect.data', 'method' => 'POST', 'id' => 's-form',])}}--}}
-{{--                        <div class="row">--}}
-{{--                            <div class="col-md-2">--}}
-{{--                                {!! Form::select('datacat_id',[' '=>'All Data']+$datacategoryid,null,['class'=>'form-control','id'=>'receiver','required']); !!}--}}
-{{--                            </div>--}}
-{{--                            <div class="col-md-4">--}}
-{{--                                <input type="text" name="search" class="form-control">--}}
-{{--                            </div>--}}
-{{--                            <div class="col-md-2">--}}
-{{--                                <button type="submit" class="btn btn-info">Search</button>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        {{ Form::close() }}--}}
-{{--                    </div>--}}
-{{--                    <div class="card-header">--}}
-{{--                        {{ Form::open(['route' => 'madmin.download.data', 'method' => 'POST', 'id' => 's-form',])}}--}}
-{{--                        <div class="row">--}}
-{{--                            <div class="col-md-2">--}}
-{{--                                {!! Form::select('datacat_id',[' '=>'All Data']+$datacategoryid,null,['class'=>'form-control','id'=>'receiver','required']); !!}--}}
-{{--                            </div>--}}
-{{--                            <div class="col-md-4">--}}
-{{--                                <input type="text" name="search" class="form-control">--}}
-{{--                            </div>--}}
-{{--                            <div class="col-md-2">--}}
-{{--                                <button type="submit" class="btn btn-info">Download</button>--}}
-{{--                            </div>--}}
-{{--                        </div>--}}
-{{--                        {{ Form::close() }}--}}
-{{--                    </div>--}}
-                    <!-- /.card-header -->
-
                         @include('Admin.include.message')
-                        <div class="card-body p-0">
-                            <table class="table table-bordered">
-                                <tbody>
-                                <tr>
-                                    <th>SI</th>
-                                    <th>MessageID</th>
-                                    <th>Subject</th>
-                                    <th>Content</th>
-                                    <th>Phone</th>
-                                    <th>Delivary</th>
-                                    <th>Status</th>
-                                    <th>Date/Time</th>
-                                </tr>
-                                @foreach($values as $value)
+
+                        <div class="card-body p-0 table-responsive">
+                            <table class="table table-bordered table-hover mb-0">
+                                <thead class="custom-thead">
                                     <tr>
-                                        <td>{{$value->id}}</td>
-                                        <td>{{$value->message_id }}</td>
-                                        <td>{{$value->subject }}</td>
-                                        <td>{{$value->content}}</td>
-                                        <td>{{$value->phone}}</td>
+                                        <th>SI</th>
+                                        <th>Message ID</th>
+                                        <th>Subject</th>
+                                        <th>Content</th>
+                                        <th>Phone</th>
+                                        <th>Delivery</th>
+                                        <th>Status</th>
+                                        <th>Date / Time</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                @forelse($values as $value)
+                                    <tr>
+                                        <td>{{ $value->id }}</td>
+                                        <td>{{ $value->message_id }}</td>
+                                        <td>{{ $value->subject }}</td>
+                                        <td>{{ $value->content }}</td>
+                                        <td>{{ $value->phone }}</td>
                                         <td>
-                                            @if($value->delivary=='DELIVRD' OR $value->delivary=='SENT')
-                                                <span class="bg bg-success p-2 rounded">
-                                                    Deliverd
-                                                </span>
-                                            @elseif($value->delivary=='ACCEPTD')
-                                                <span class="bg bg-warning p-2 rounded">
-                                                    Pending
-                                                </span>
-                                            @endif
-                                        </td>
-                                        <td>
-                                            @if($value->status==0)
-                                                <span class="bg bg-info p-2 rounded">
-                                                    If request successful(Acceptd)
-                                                </span>
-                                            @elseif($value->status==1)
-                                                <span class="bg bg-danger p-2 rounded">
-                                                    If request failed (REJECTD)
-                                                </span>
-                                            @elseif($value->status==101)
-                                                <span class="bg bg-danger p-2 rounded">
-                                                    if internal server error occurs (REJECTD)
-                                                </span>
-                                            @elseif($value->status==114)
-                                                <span class="bg bg-danger p-2 rounded">
-                                                    If content not provided (REJECTD)
-                                                </span>
-                                            @elseif($value->status==108)
-                                                <span class="bg bg-danger p-2 rounded">
-                                                    If wrong password/ not provided (REJECTD)
-                                                </span>
-                                            @elseif($value->status==109)
-                                                <span class="bg bg-danger p-2 rounded">
-                                                    If user not provided/Deleted (REJECTD)
-                                                </span>
+                                            @if(in_array($value->delivary, ['DELIVRD', 'SENT']))
+                                                <span class="badge badge-success">Delivered</span>
+                                            @elseif($value->delivary === 'ACCEPTD')
+                                                <span class="badge badge-warning">Pending</span>
                                             @else
-                                                <span class="bg bg-danger p-2 rounded">
-                                                    REJECTD
-                                                </span>
+                                                <span class="badge badge-secondary">{{ $value->delivary }}</span>
                                             @endif
                                         </td>
                                         <td>
-                                            {{ date("d-m-Y", strtotime($value->created_at)) }} /{{ date("H:i:s", strtotime($value->created_at)) }}
+                                            @switch($value->status)
+                                                @case(0)
+                                                    <span class="badge badge-info">Request Successful (Accepted)</span>
+                                                    @break
+                                                @case(1)
+                                                    <span class="badge badge-danger">Request Failed (Rejected)</span>
+                                                    @break
+                                                @case(101)
+                                                    <span class="badge badge-danger">Internal Server Error (Rejected)</span>
+                                                    @break
+                                                @case(114)
+                                                    <span class="badge badge-danger">Content Not Provided (Rejected)</span>
+                                                    @break
+                                                @case(108)
+                                                    <span class="badge badge-danger">Wrong Password / Not Provided (Rejected)</span>
+                                                    @break
+                                                @case(109)
+                                                    <span class="badge badge-danger">User Not Provided / Deleted (Rejected)</span>
+                                                    @break
+                                                @default
+                                                    <span class="badge badge-danger">Rejected</span>
+                                            @endswitch
+                                        </td>
+                                        <td>
+                                            {{ $value->created_at->format('d-m-Y / H:i:s') }}
                                         </td>
                                     </tr>
-                                @endforeach
-
+                                @empty
+                                    <tr>
+                                        <td colspan="8" class="text-center">No SMS logs found.</td>
+                                    </tr>
+                                @endforelse
                                 </tbody>
                             </table>
-
                         </div>
                         <!-- /.card-body -->
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 offset-md-2 m-0 float-right">
-                            {{ $values->render() }}
+
+                        <div class="card-footer clearfix">
+                            {{ $values->links() }}
                         </div>
                     </div>
                 </div>
             </div>
-            <!-- /.row (main row) -->
+            <!-- /.row -->
         </div><!-- /.container-fluid -->
     </section>
     <!-- /.content -->
