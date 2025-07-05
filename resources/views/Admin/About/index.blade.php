e35cc21f267fd3f066fa1168c64134433ef0d00f
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 13:55:00 +0600

    about design change

--- resources/views/Admin/About/index.blade.php
+++ resources/views/Admin/About/index.blade.php
@@ -21,53 +21,53 @@
                         @include('Admin.include.message')
 
                         <div class="card-body p-0">
-                            <table class="table table-bordered">
-                                <thead class="custom-thead">
-                                    <tr>
-                                        <th>About Title</th>
-                                        <th>Content</th>
-                                        <th>Mission</th>
-                                        <th>Vision</th>
-                                        <th>Establishment</th>
-                                        <th>Date</th>
-                                        <th>Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($categories as $value)
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover mb-0">
+                                    <thead class="custom-thead">
                                         <tr>
-                                            <td>{{ $value->about_title }}</td>
-                                            <td>{{ $value->content }}</td>
-                                            <td>{{ $value->mission }}</td>
-                                            <td>{{ $value->vision }}</td>
-                                            <td>{{ $value->establistmet }}</td>
-                                            <td>{{ $value->created_at->diffForHumans() }}</td>
-                                            <td>
-                                                <div class="d-flex flex-wrap">
+                                            <th>About Title</th>
+                                            <th>Content</th>
+                                            <th class="d-none d-md-table-cell">Mission</th>
+                                            <th class="d-none d-md-table-cell">Vision</th>
+                                            <th class="d-none d-md-table-cell">Establishment</th>
+                                            <th>Date</th>
+                                            <th class="text-nowrap">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($categories as $value)
+                                            <tr>
+                                                <td>{{ $value->about_title }}</td>
+                                                <td>{{ Str::limit(strip_tags($value->content), 100) }}</td>
+                                                <td class="d-none d-md-table-cell">{{ $value->mission }}</td>
+                                                <td class="d-none d-md-table-cell">{{ $value->vision }}</td>
+                                                <td class="d-none d-md-table-cell">{{ $value->establistmet }}</td>
+                                                <td>{{ $value->created_at->diffForHumans() }}</td>
+                                                <td class="text-nowrap">
                                                     <a href="{{ route('madmin.aboutadmin.edit', $value->id) }}"
-                                                       class="btn btn-success m-1">
+                                                       class="btn btn-success btn-sm m-1">
                                                         <i class="lni-pencil-alt"></i>
                                                     </a>
                                                     {{-- Uncomment if you want to enable delete --}}
                                                     {{--
-                                                    {!! Form::open(['method' => 'DELETE', 'route' => ['madmin.categories.destroy', $value->id]]) !!}
-                                                        <button type="submit" class="btn btn-danger m-1"
+                                                    {!! Form::open(['method' => 'DELETE', 'route' => ['madmin.categories.destroy', $value->id], 'style' => 'display:inline']) !!}
+                                                        <button type="submit" class="btn btn-danger btn-sm m-1"
                                                                 onclick="return confirm('Do you want to delete this item? This will also delete associated products.')">
                                                             <i class="lni-trash"></i>
                                                         </button>
                                                     {!! Form::close() !!}
                                                     --}}
-                                                </div>
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
 
                         <div class="card-footer clearfix">
                             <div class="float-right">
-                                {{ $categories->render() }}
+                                {{ $categories->links() }}
                             </div>
                         </div>
 
--- resources/views/Admin/Blogs/add.blade.php
+++ resources/views/Admin/Blogs/add.blade.php
@@ -1,23 +1,14 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Add Blog</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ url('slide') }}">Blog</a></li>
-                        <li class="breadcrumb-item active">Add Blog</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    @include('Admin.include.breadcrumb', [
+        'page' => 'Add Blog',
+        'parent' => 'Blog',
+        'child' => 'Add Blog',
+        'button' => 'Blog List',
+        'button_icon' => 'lni lni-list',
+        'route' => route('madmin.blogs.index'),
+    ])
 
     <!-- Main content -->
     <section class="content">
@@ -28,73 +19,83 @@
                     <div class="card card-info">
                         @include('Admin.include.message')
                         <!-- form start -->
-                        {!! Form::open(['method'=>'POST','route'=>'madmin.blogs.store','class'=>'form-horizontal', 'files'=>true]) !!}
+                        {!! Form::open([
+                            'method' => 'POST',
+                            'route' => 'madmin.blogs.store',
+                            'class' => 'form-horizontal',
+                            'files' => true,
+                        ]) !!}
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Blog Title', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('title', null, ['class'=>'form-control','id'=>'receiver','required']) !!}
+                                            {!! Form::text('title', null, ['class' => 'form-control', 'id' => 'receiver', 'required']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Image', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::file('images', ['class'=>'form-control','id'=>'receiver','required']) !!}
+                                            {!! Form::file('images', ['class' => 'form-control', 'id' => 'receiver', 'required']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Content', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::textarea('content', null, ['class'=>'form-control','id'=>'summernote']) !!}
+                                            {!! Form::textarea('content', null, ['class' => 'form-control', 'id' => 'summernote']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Blog Writer', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('writer', null, ['class'=>'form-control','id'=>'receiver','required']) !!}
+                                            {!! Form::text('writer', null, ['class' => 'form-control', 'id' => 'receiver', 'required']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Status', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::select('status', ['Active' => 'Active','Pending' => 'Pending'],null,['class'=>'form-control','id'=>'receiver','required']); !!}
+                                            {!! Form::select('status', ['Active' => 'Active', 'Pending' => 'Pending'], null, [
+                                                'class' => 'form-control',
+                                                'id' => 'receiver',
+                                                'required',
+                                            ]) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Meta Title', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('meta_title', null, ['class'=>'form-control','id'=>'receiver']) !!}
+                                            {!! Form::text('meta_title', null, ['class' => 'form-control', 'id' => 'receiver']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Meta Keyword', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('meta_keyword', null, ['class'=>'form-control','id'=>'receiver']) !!}
+                                            {!! Form::text('meta_keyword', null, ['class' => 'form-control', 'id' => 'receiver']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Meta Description', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('meta_description', null, ['class'=>'form-control','id'=>'receiver']) !!}
+                                            {!! Form::text('meta_description', null, ['class' => 'form-control', 'id' => 'receiver']) !!}
                                         </div>
                                     </div>
 
                                 </div>
                             </div>
+                            <div class="mt-3">
+                                <button type="submit" class="btn btn-info">Submit</button>
+                                <button onclick="window.history.back()" class="btn btn-default">Cancel</button>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-info">Submit</button>
-                            <button  onclick="window.history.back()" class="btn btn-default float-right">Cancel</button>
-                        </div>
+
                         {!! Form::close() !!}
                     </div>
                 </div>
@@ -105,9 +106,9 @@
     <!-- /.content -->
 @endsection
 @section('script')
-<script type="text/javascript">
-    $(document).ready(function() {
-      $('#summernote').summernote();
-    });
-</script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>
 @endsection
