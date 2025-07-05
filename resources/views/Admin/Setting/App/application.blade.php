c13e3ffa86c5c676316c3fae6de2ebf80b7b6c30
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 13:53:32 +0600

    applicatin design change

--- resources/views/Admin/Setting/App/application.blade.php
+++ resources/views/Admin/Setting/App/application.blade.php
@@ -1,11 +1,12 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <h1 class="m-0 text-dark">System Settings</h1>
-        </div>
-    </div>
+    @include('Admin.include.breadcrumb', [
+        'page' => __('System Settings'),
+        'parent' => __('Home'),
+        'child' => __('Settings'),
+        'route' => '#',
+    ])
 
     <section class="content">
         <div class="container-fluid">
@@ -17,7 +18,7 @@
                     'method' => 'POST',
                     'route' => 'madmin.appsetting.store',
                     'files' => true,
-                    'class' => 'form-horizontal'
+                    'class' => 'form-horizontal',
                 ]) !!}
 
                 {!! Form::hidden('id', $setting->id ?? null) !!}
@@ -35,11 +36,11 @@
                     {{-- General Info --}}
                     <div class="tab-pane fade show active" id="general">
                         @foreach ([
-                            'site_title' => 'Site Title',
-                            'store_name' => 'Store Name',
-                            'email' => 'Email',
-                            'phone' => 'Phone',
-                        ] as $name => $label)
+            'site_title' => 'Site Title',
+            'store_name' => 'Store Name',
+            'email' => 'Email',
+            'phone' => 'Phone',
+        ] as $name => $label)
                             <div class="form-group row">
                                 {!! Form::label($name, $label, ['class' => 'col-sm-3 col-form-label']) !!}
                                 <div class="col-sm-9">
@@ -62,10 +63,9 @@
                             {!! Form::label('logo', 'Logo', ['class' => 'col-sm-3 col-form-label']) !!}
                             <div class="col-sm-9">
                                 {!! Form::file('logo', ['class' => 'form-control', 'onchange' => "previewImage(this, 'logoPreview')"]) !!}
-                                <img id="logoPreview"
-                                     src="{{ isset($setting->logo) ? asset($setting->logo) : '' }}"
-                                     class="img-thumbnail mt-2"
-                                     style="{{ isset($setting->logo) ? '' : 'display:none;' }} max-height:100px;" />
+                                <img id="logoPreview" src="{{ isset($setting->logo) ? asset($setting->logo) : '' }}"
+                                    class="img-thumbnail mt-2"
+                                    style="{{ isset($setting->logo) ? '' : 'display:none;' }} max-height:100px;" />
                             </div>
                         </div>
 
@@ -74,9 +74,9 @@
                             <div class="col-sm-9">
                                 {!! Form::file('favicon', ['class' => 'form-control', 'onchange' => "previewImage(this, 'faviconPreview')"]) !!}
                                 <img id="faviconPreview"
-                                     src="{{ isset($setting->favicon) ? asset($setting->favicon) : '' }}"
-                                     class="img-thumbnail mt-2"
-                                     style="{{ isset($setting->favicon) ? '' : 'display:none;' }} max-height:50px;" />
+                                    src="{{ isset($setting->favicon) ? asset($setting->favicon) : '' }}"
+                                    class="img-thumbnail mt-2"
+                                    style="{{ isset($setting->favicon) ? '' : 'display:none;' }} max-height:50px;" />
                             </div>
                         </div>
 
@@ -93,23 +93,30 @@
                         <div class="form-group row">
                             {!! Form::label('invoice_company', 'Company Name (Invoice)', ['class' => 'col-sm-3 col-form-label']) !!}
                             <div class="col-sm-9">
-                                {!! Form::text('invoice_company', old('invoice_company', $setting->invoice_company ?? ''), ['class' => 'form-control']) !!}
+                                {!! Form::text('invoice_company', old('invoice_company', $setting->invoice_company ?? ''), [
+                                    'class' => 'form-control',
+                                ]) !!}
                             </div>
                         </div>
                         <div class="form-group row">
                             {!! Form::label('invoice_email', 'Invoice Email', ['class' => 'col-sm-3 col-form-label']) !!}
                             <div class="col-sm-9">
-                                {!! Form::email('invoice_email', old('invoice_email', $setting->invoice_email ?? ''), ['class' => 'form-control']) !!}
+                                {!! Form::email('invoice_email', old('invoice_email', $setting->invoice_email ?? ''), [
+                                    'class' => 'form-control',
+                                ]) !!}
                             </div>
                         </div>
                         <div class="form-group row">
                             {!! Form::label('invoice_logo', 'Invoice Logo', ['class' => 'col-sm-3 col-form-label']) !!}
                             <div class="col-sm-9">
-                                {!! Form::file('invoice_logo', ['class' => 'form-control', 'onchange' => "previewImage(this, 'invoiceLogoPreview')"]) !!}
+                                {!! Form::file('invoice_logo', [
+                                    'class' => 'form-control',
+                                    'onchange' => "previewImage(this, 'invoiceLogoPreview')",
+                                ]) !!}
                                 <img id="invoiceLogoPreview"
-                                     src="{{ isset($setting->invoice_logo) ? asset($setting->invoice_logo) : '' }}"
-                                     class="img-thumbnail mt-2"
-                                     style="{{ isset($setting->invoice_logo) ? '' : 'display:none;' }} max-height:100px;" />
+                                    src="{{ isset($setting->invoice_logo) ? asset($setting->invoice_logo) : '' }}"
+                                    class="img-thumbnail mt-2"
+                                    style="{{ isset($setting->invoice_logo) ? '' : 'display:none;' }} max-height:100px;" />
                             </div>
                         </div>
                     </div>
@@ -117,13 +124,13 @@
                     {{-- Location --}}
                     <div class="tab-pane fade" id="location">
                         @foreach ([
-                            'country' => 'Country',
-                            'latitude' => 'Latitude',
-                            'longitude' => 'Longitude',
-                            // 'currency_id' => 'Currency ID',
-                            'language' => 'Language',
-                            'timezone' => 'Timezone',
-                        ] as $name => $label)
+            'country' => 'Country',
+            'latitude' => 'Latitude',
+            'longitude' => 'Longitude',
+            // 'currency_id' => 'Currency ID',
+            'language' => 'Language',
+            'timezone' => 'Timezone',
+        ] as $name => $label)
                             <div class="form-group row">
                                 {!! Form::label($name, $label, ['class' => 'col-sm-3 col-form-label']) !!}
                                 <div class="col-sm-9">
@@ -136,7 +143,8 @@
                             <div class="col-sm-9">
                                 <select name="currency_id" id="currency_id" class="form-control">
                                     @foreach ($currencies as $currency)
-                                        <option value="{{ $currency->id }}" {{ (old('currency_id', $setting->currency_id ?? '') == $currency->id) ? 'selected' : '' }}>
+                                        <option value="{{ $currency->id }}"
+                                            {{ old('currency_id', $setting->currency_id ?? '') == $currency->id ? 'selected' : '' }}>
                                             {{ $currency->title }}
                                         </option>
                                     @endforeach
@@ -146,20 +154,24 @@
                         <div class="form-group row">
                             {!! Form::label('google_map_embed_link', 'Google Map Embed Link', ['class' => 'col-sm-3 col-form-label']) !!}
                             <div class="col-sm-9">
-                                {!! Form::textarea('google_map_embed_link', old('google_map_embed_link', $setting->google_map_embed_link ?? ''), ['class' => 'form-control']) !!}
+                                {!! Form::textarea(
+                                    'google_map_embed_link',
+                                    old('google_map_embed_link', $setting->google_map_embed_link ?? ''),
+                                    ['class' => 'form-control'],
+                                ) !!}
                             </div>
                         </div>
                     </div>
-                     {{-- social media --}}
+                    {{-- social media --}}
                     <div class="tab-pane fade" id="social">
                         @foreach ([
-                            'facebook' => 'Facebook',
-                            'twitter' => 'Twitter',
-                            'linkedin' => 'LinkedIn',
-                            'instagram' => 'Instagram',
-                            'youtube' => 'YouTube',
-                            'tiktok' => 'TikTok',
-                        ] as $name => $label)
+            'facebook' => 'Facebook',
+            'twitter' => 'Twitter',
+            'linkedin' => 'LinkedIn',
+            'instagram' => 'Instagram',
+            'youtube' => 'YouTube',
+            'tiktok' => 'TikTok',
+        ] as $name => $label)
                             <div class="form-group row">
                                 {!! Form::label($name, $label, ['class' => 'col-sm-3 col-form-label']) !!}
                                 <div class="col-sm-9">
@@ -172,10 +184,10 @@
                     {{-- Other --}}
                     <div class="tab-pane fade" id="other">
                         @foreach ([
-                            'date_format' => 'Date Format',
-                            'site_alignment' => 'Site Alignment',
-                            'footer_text' => 'Footer Text',
-                        ] as $name => $label)
+            'date_format' => 'Date Format',
+            'site_alignment' => 'Site Alignment',
+            'footer_text' => 'Footer Text',
+        ] as $name => $label)
                             <div class="form-group row">
                                 {!! Form::label($name, $label, ['class' => 'col-sm-3 col-form-label']) !!}
                                 <div class="col-sm-9">
@@ -187,7 +199,9 @@
                         <div class="form-group row">
                             {!! Form::label('powered_by_text', 'Powered By Text', ['class' => 'col-sm-3 col-form-label']) !!}
                             <div class="col-sm-9">
-                                {!! Form::textarea('powered_by_text', old('powered_by_text', $setting->powered_by_text ?? ''), ['class' => 'form-control']) !!}
+                                {!! Form::textarea('powered_by_text', old('powered_by_text', $setting->powered_by_text ?? ''), [
+                                    'class' => 'form-control',
+                                ]) !!}
                             </div>
                         </div>
                     </div>
@@ -205,18 +219,18 @@
 @endsection
 
 @section('script')
-<script>
-    function previewImage(input, previewId) {
-        const file = input.files[0];
-        const reader = new FileReader();
-        reader.onload = function(e) {
-            const preview = document.getElementById(previewId);
-            preview.src = e.target.result;
-            preview.style.display = 'block';
-        };
-        if (file) {
-            reader.readAsDataURL(file);
+    <script>
+        function previewImage(input, previewId) {
+            const file = input.files[0];
+            const reader = new FileReader();
+            reader.onload = function(e) {
+                const preview = document.getElementById(previewId);
+                preview.src = e.target.result;
+                preview.style.display = 'block';
+            };
+            if (file) {
+                reader.readAsDataURL(file);
+            }
         }
-    }
-</script>
+    </script>
 @endsection
