ae13c40ce4b48a39a56f0e6a1d89444e1ff9fa32
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 13:52:26 +0600

    language design change

--- resources/views/Admin/Setting/Language/edit.blade.php
+++ resources/views/Admin/Setting/Language/edit.blade.php
@@ -1,66 +1,72 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row ">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Edit Language: {{ $locale }}</h1>
+    {{-- Breadcrumb --}}
+    @include('Admin.include.breadcrumb', [
+        'page' => __('Edit Language') . ': ' . $locale,
+        'parent' => __('Setting'),
+        'child' => __('Edit Languages'),
+        'button' => __('Back'),
+        'button_icon' => 'fa fa-arrow-left',
+        'route' => route('madmin.lang.index'),
+    ])
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @include('Admin.include.message')
+
+                <div class="card">
+                    <div class="card-body">
+                        <form method="POST" action="{{ route('madmin.lang.update', $locale) }}">
+                            @csrf
+
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover" id="translation-table">
+                                    <thead class="thead-light">
+                                        <tr>
+                                            <th style="width: 40%">Key</th>
+                                            <th>Translation</th>
+                                            <th style="width: 10%">Action</th>
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($translations as $key => $value)
+                                            <tr>
+                                                <td>
+                                                    <input type="text" name="keys[]" value="{{ $key }}"
+                                                        class="form-control" readonly>
+                                                </td>
+                                                <td>
+                                                    <input type="text" name="translations[{{ $key }}]"
+                                                        value="{{ $value }}" class="form-control">
+                                                </td>
+                                                <td>
+                                                    @if ($loop->last)
+                                                        <button type="button" class="btn btn-secondary btn-sm"
+                                                            onclick="addNewRow()">
+                                                            <i class="fas fa-plus"></i>
+                                                        </button>
+                                                    @endif
+                                                </td>
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
+
+                            <div class="mt-3">
+                                <button class="btn btn-success">Save Changes</button>
+                                <a href="{{ route('madmin.lang.index') }}" class="btn btn-secondary">Back</a>
+                            </div>
+                        </form>
+                    </div>
                 </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <a href="#" class="text-primary mr-2">Setting</a>
-                        <a href="#" class="text-success btn-sm mr-2">/Edit Languages</a>
-                    </ol>
-                </div>
+
             </div>
         </div>
     </div>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="card">
-                <div class="card-body">
-                    @include('Admin.include.message')
-                    <form method="POST" action="{{ route('madmin.lang.update', $locale) }}">
-                        @csrf
-                        <div class="table-responsive">
-                            <table class="table table-bordered" id="translation-table">
-                                <thead>
-                                    <tr>
-                                        <th style="width: 40%">Key</th>
-                                        <th>Translation</th>
-                                        <th style="width: 10%">Action</th>
-                                    </tr>
-                                </thead>
-                                <tbody>
-                                    @foreach ($translations as $key => $value)
-                                        <tr>
-                                            <td><input type="text" name="keys[]" value="{{ $key }}"
-                                                    class="form-control" readonly></td>
-                                            <td><input type="text" name="translations[{{ $key }}]"
-                                                    value="{{ $value }}" class="form-control"></td>
-                                            <td>
-                                                {{-- if $key is last then show --}}
-                                                @if ($loop->last)
-                                                    <button type="button" class="btn btn-secondary btn-sm" onclick="addNewRow()"><i class="fas fa-plus"></i></button>
-                                                @endif
-                                            </td>
-                                        </tr>
-                                    @endforeach
-                                </tbody>
-                            </table>
-                        </div>
 
-                        {{-- <button type="button" class="btn btn-sm btn-secondary mb-3" onclick="addNewRow()"> + Add New </button> --}}
-                        <div>
-                            <button class="btn btn-success">Save Changes</button>
-                            <a href="{{ route('madmin.lang.index') }}" class="btn btn-secondary">Back</a>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div>
-    </section>
     <script>
         function addNewRow() {
             const table = document.getElementById('translation-table').getElementsByTagName('tbody')[0];
--- resources/views/Admin/Setting/Language/index.blade.php
+++ resources/views/Admin/Setting/Language/index.blade.php
@@ -1,90 +1,60 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row ">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Available Languages</h1>
+    {{-- Breadcrumb --}}
+    @include('Admin.include.breadcrumb', [
+        'page' => __('Available Languages'),
+        'parent' => __('Setting'),
+        'child' => __('Languages'),
+        'button' => null,
+        'button_icon' => null,
+        'route' => '#',
+    ])
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                @include('Admin.include.message')
+
+                <div class="card">
+                    <div class="card-body">
+                        <div class="table-responsive">
+                            <table class="table table-bordered table-striped mb-0">
+                                <thead class="custom-thead">
+                                    <tr>
+                                        <th>Locale</th>
+                                        <th>File Name</th>
+                                        <th>Size (bytes)</th>
+                                        <th>Last Modified</th>
+                                        <th class="text-nowrap">Action</th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    @forelse ($languages as $lang)
+                                        <tr>
+                                            <td>{{ $lang['locale'] }}</td>
+                                            <td>{{ $lang['name'] }}</td>
+                                            <td>{{ $lang['size'] }}</td>
+                                            <td>{{ date('Y-m-d H:i:s', $lang['last_modified']) }}</td>
+                                            <td>
+                                                <a href="{{ route('madmin.lang.edit', $lang['locale']) }}"
+                                                    class="btn btn-sm btn-primary">
+                                                    <i class="fa fa-edit"></i> Edit
+                                                </a>
+                                            </td>
+                                        </tr>
+                                    @empty
+                                        <tr>
+                                            <td colspan="5" class="text-center">No language files found.</td>
+                                        </tr>
+                                    @endforelse
+                                </tbody>
+                            </table>
+                        </div> <!-- /.table-responsive -->
+                    </div>
                 </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                            <a href="#" class="text-primary mr-2">Setting</a>
-                            <a href="#" class="text-success btn-sm mr-2">/Languages</a>
-                    </ol>
-                </div>
+
             </div>
         </div>
     </div>
-
-    <!-- Main Content -->
-    <section class="content">
-        <div class="container-fluid">
-            <div class="card">
-
-                <div class="card-body">
-                    @include('Admin.include.message')
-
-                    <div class="table-responsive">
-                        <table class="table table-bordered">
-                            <thead>
-                                <tr>
-                                    <th>Locale</th>
-                                    <th>File Name</th>
-                                    <th>Size (bytes)</th>
-                                    <th>Last Modified</th>
-                                    <th>Action</th>
-                                </tr>
-                            </thead>
-                            <tbody>
-                                @foreach ($languages as $lang)
-                                    <tr>
-                                        <td>{{ $lang['locale'] }}</td>
-                                        <td>{{ $lang['name'] }}</td>
-                                        <td>{{ $lang['size'] }}</td>
-                                        <td>{{ date('Y-m-d H:i:s', $lang['last_modified']) }}</td>
-                                        <td><a href="{{ route('madmin.lang.edit', $lang['locale']) }}"
-                                                class="btn btn-sm btn-primary">Edit</a></td>
-                                    </tr>
-                                @endforeach
-                            </tbody>
-                        </table>
-
-
-                    </div>
-                </div>
-            </div>
-        </div>
-    </section>
 @endsection
-
-
-{{-- @extends('Admin.layoutApp.app')
-
-@section('content')
-<div class="container mt-4">
-    <h3>Available Languages</h3>
-    <table class="table table-bordered">
-        <thead>
-            <tr>
-                <th>Locale</th>
-                <th>File Name</th>
-                <th>Size (bytes)</th>
-                <th>Last Modified</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach ($languages as $lang)
-                <tr>
-                    <td>{{ $lang['locale'] }}</td>
-                    <td>{{ $lang['name'] }}</td>
-                    <td>{{ $lang['size'] }}</td>
-                    <td>{{ date('Y-m-d H:i:s', $lang['last_modified']) }}</td>
-                    <td><a href="{{ route('madmin.lang.edit', $lang['locale']) }}" class="btn btn-sm btn-primary">Edit</a></td>
-                </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
-@endsection --}}
