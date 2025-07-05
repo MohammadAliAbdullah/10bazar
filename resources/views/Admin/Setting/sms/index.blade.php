3f118633d632fd8d9eb2535d3df01f78ee58e6c5
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 13:51:11 +0600

    sms design change

--- resources/views/Admin/Setting/sms/index.blade.php
+++ resources/views/Admin/Setting/sms/index.blade.php
@@ -1,88 +1,97 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row ">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">SMS Configuration</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item">SMS</li>
-                    </ol>
+    {{-- Breadcrumb --}}
+    @include('Admin.include.breadcrumb', [
+        'page' => __('SMS Configuration'),
+        'parent' => __('Home'),
+        'child' => __('SMS'),
+        'button' => __('Back'),
+        'button_icon' => 'lni lni-arrow-left',
+        'route' => '#', // change route if needed
+    ])
+
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-12">
+                <div class="card">
+                    <div class="card-body">
+                        @include('Admin.include.message')
+
+                        {!! Form::open([
+                            'method' => 'POST',
+                            'route' => 'madmin.smsconfig.store',
+                            'class' => 'form-horizontal',
+                        ]) !!}
+                        @csrf
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                {!! Form::label('provider', 'Provider Name') !!} <span class="text-danger">*</span>
+                                {!! Form::text('provider', old('provider', $sms->provider ?? ''), ['class' => 'form-control', 'required']) !!}
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                {!! Form::label('username', 'User Name (API Key)') !!} <span class="text-danger">*</span>
+                                {!! Form::text('username', old('username', $sms->username ?? ''), ['class' => 'form-control', 'required']) !!}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                {!! Form::label('password', 'Password (API Secret)') !!} <span class="text-danger">*</span>
+                                {!! Form::text('password', old('password', $sms->password ?? ''), ['class' => 'form-control', 'required']) !!}
+                            </div>
+
+                            <div class="form-group col-md-6">
+                                {!! Form::label('phone', 'Phone') !!}
+                                {!! Form::text('phone', old('phone', $sms->phone ?? ''), ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                {!! Form::label('sender_name', 'Sender Name') !!}
+                                {!! Form::text('sender_name', old('sender_name', $sms->sender_name ?? ''), ['class' => 'form-control']) !!}
+                            </div>
+                        </div>
+
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                {!! Form::label('is_invoice', 'Sell') !!}
+                                <div class="d-flex gap-3">
+                                    <label class="btn btn-outline-secondary">
+                                        {!! Form::radio('is_invoice', 1, old('is_invoice', $sms->is_invoice ?? 0) == 1) !!} Yes
+                                    </label>
+                                    <label class="btn btn-outline-success">
+                                        {!! Form::radio('is_invoice', 0, old('is_invoice', $sms->is_invoice ?? 0) == 0) !!} No
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row">
+                            <div class="form-group col-md-6">
+                                {!! Form::label('is_receive', 'Customer Receive') !!}
+                                <div class="d-flex gap-3">
+                                    <label class="btn btn-outline-secondary">
+                                        {!! Form::radio('is_receive', 1, old('is_receive', $sms->is_receive ?? 0) == 1) !!} Yes
+                                    </label>
+                                    <label class="btn btn-outline-success">
+                                        {!! Form::radio('is_receive', 0, old('is_receive', $sms->is_receive ?? 0) == 0) !!} No
+                                    </label>
+                                </div>
+                            </div>
+                        </div>
+
+                        <div class="mt-3">
+                            <button type="submit" class="btn btn-primary">Save Settings</button>
+                            <button onclick="window.history.back()" type="button" class="btn btn-default">Cancel</button>
+                        </div>
+
+                        {!! Form::close() !!}
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-    <section class="content">
-        <div class="container-fluid">
-            <div class="card shadow-sm">
-                @include('Admin.include.message')
-                <form action="{{ route('madmin.smsconfig.store') }}" method="POST" class="p-3 form-horizontal">
-                    @csrf
-
-                    <div class="form-group">
-                        <label>Provider Name <span class="text-danger">*</span></label>
-                        <input type="text" name="provider" class="form-control"
-                            value="{{ old('provider', $sms->provider ?? '') }}" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label>User Name (API Key) <span class="text-danger">*</span></label>
-                        <input type="text" name="username" class="form-control"
-                            value="{{ old('username', $sms->username ?? '') }}" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label>Password (API Secret) <span class="text-danger">*</span></label>
-                        <input type="text" name="password" class="form-control"
-                            value="{{ old('password', $sms->password ?? '') }}" required>
-                    </div>
-
-                    <div class="form-group">
-                        <label>Phone</label>
-                        <input type="text" name="phone" class="form-control"
-                            value="{{ old('phone', $sms->phone ?? '') }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label>Sender Name</label>
-                        <input type="text" name="sender_name" class="form-control"
-                            value="{{ old('sender_name', $sms->sender_name ?? '') }}">
-                    </div>
-
-                    <div class="form-group">
-                        <label>Sell</label>
-                        <div>
-                            <label class="btn btn-secondary">
-                                <input type="radio" name="is_invoice" value="1"
-                                    {{ old('is_invoice', $sms->is_invoice ?? 0) == 1 ? 'checked' : '' }}> Yes
-                            </label>
-                            <label class="btn btn-success">
-                                <input type="radio" name="is_invoice" value="0"
-                                    {{ old('is_invoice', $sms->is_invoice ?? 0) == 0 ? 'checked' : '' }}> No
-                            </label>
-                        </div>
-                    </div>
-
-                    <div class="form-group">
-                        <label>Customer Receive</label>
-                        <div>
-                            <label class="btn btn-secondary">
-                                <input type="radio" name="is_receive" value="1"
-                                    {{ old('is_receive', $sms->is_receive ?? 0) == 1 ? 'checked' : '' }}> Yes
-                            </label>
-                            <label class="btn btn-success">
-                                <input type="radio" name="is_receive" value="0"
-                                    {{ old('is_receive', $sms->is_receive ?? 0) == 0 ? 'checked' : '' }}> No
-                            </label>
-                        </div>
-                    </div>
-
-                    <button type="submit" class="btn btn-primary mt-2">Save Settings</button>
-                </form>
-            </div>
-        </div>
-    </section>
 @endsection
