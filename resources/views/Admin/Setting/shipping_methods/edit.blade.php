8ca65a0b827d870804f6563e8313888d6f6ee23c
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 13:51:42 +0600

    shipping method design change

--- resources/views/Admin/Setting/shipping_methods/edit.blade.php
+++ resources/views/Admin/Setting/shipping_methods/edit.blade.php
@@ -1,21 +1,14 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Edit Shipping Method</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ route('madmin.shipping-methods.index') }}">Shipping Methods</a></li>
-                        <li class="breadcrumb-item active">Edit</li>
-                    </ol>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('Admin.include.breadcrumb', [
+        'page' => 'Edit Shipping Method',
+        'parent' => 'Home',
+        'child' => 'Shipping Methods',
+        'button' => 'Back to List',
+        'button_icon' => 'lni lni-arrow-left',
+        'route' => route('madmin.shipping-methods.index'),
+    ])
 
     <section class="content">
         <div class="container-fluid">
--- resources/views/Admin/Setting/shipping_methods/index.blade.php
+++ resources/views/Admin/Setting/shipping_methods/index.blade.php
@@ -1,22 +1,14 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Shipping Methods</h1>
-                </div>
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <a href="{{ route('madmin.shipping-methods.create') }}" class="btn btn-primary btn-sm mr-2">
-                            <i class="fa fa-plus"></i> Add Shipping Method
-                        </a>
-                    </ol>
-                </div>
-            </div>
-        </div>
-    </div>
+    @include('Admin.include.breadcrumb', [
+        'page' => 'Shipping Methods',
+        'parent' => 'Home',
+        'child' => 'Shipping Methods',
+        'button' => 'Add Shipping Method',
+        'button_icon' => 'lni lni-plus',
+        'route' => route('madmin.shipping-methods.create'),
+    ])
 
     <section class="content">
         <div class="container-fluid">
@@ -25,7 +17,7 @@
             <div class="card">
                 <div class="card-body">
                     <table class="table table-bordered">
-                        <thead>
+                        <thead class="custom-thead">
                             <tr>
                                 <th>ID</th>
                                 <th>Name</th>
@@ -48,11 +40,11 @@
                                     <td>{{ $shippingMethod->estimated_days }}</td>
                                     <td>{{ $shippingMethod->is_active ? 'Active' : 'Inactive' }}</td>
                                     <td>
-                                        <a href="{{ route('madmin.shipping-methods.edit', $shippingMethod->id) }}" class="btn btn-info btn-sm">Edit</a>
+                                        <a href="{{ route('madmin.shipping-methods.edit', $shippingMethod->id) }}" class="btn btn-info btn-sm"><i class="lni lni-pencil-alt"></i></a>
                                         <form action="{{ route('madmin.shipping-methods.destroy', $shippingMethod->id) }}" method="POST" style="display: inline-block;">
                                             @csrf
                                             @method('DELETE')
-                                            <button type="submit" class="btn btn-danger btn-sm">Delete</button>
+                                            <button type="submit" class="btn btn-danger btn-sm"><i class="lni lni-trash"></i></button>
                                         </form>
                                     </td>
                                 </tr>
