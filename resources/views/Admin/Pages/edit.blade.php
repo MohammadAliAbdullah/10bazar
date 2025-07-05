a6f816adab001afad7a217e0e6c29323010b62ea
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 13:54:12 +0600

    page design change

--- resources/views/Admin/Pages/edit.blade.php
+++ resources/views/Admin/Pages/edit.blade.php
@@ -1,23 +1,14 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Edit Page</h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="{{ url('slide') }}">Page</a></li>
-                        <li class="breadcrumb-item active">Edit Page</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
+    @include('Admin.include.breadcrumb', [
+        'page' => __('Edit Page'),
+        'parent' => __('Home'),
+        'child' => __('Page'),
+        'button' => __('Page List'),
+        'button_icon' => 'lni lni-list',
+        'route' => route('madmin.pages.index'),
+    ])
 
     <!-- Main content -->
     <section class="content">
@@ -25,81 +16,93 @@
             <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-md-12">
-                    <div class="card card-info">
+                    <div class="card">
                         @include('Admin.include.message')
                         <!-- form start -->
-                        {!! Form::model($page, ['method'=>'PATCH','route'=>['madmin.pages.update', $page->id],'class'=>'form-horizontal', 'files'=>true]) !!}
+                        {!! Form::model($page, [
+                            'method' => 'PATCH',
+                            'route' => ['madmin.pages.update', $page->id],
+                            'class' => 'form-horizontal',
+                            'files' => true,
+                        ]) !!}
                         <div class="card-body">
                             <div class="row">
                                 <div class="col-md-12">
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Blog Title', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('title', null, ['class'=>'form-control','id'=>'receiver','required']) !!}
+                                            {!! Form::text('title', null, ['class' => 'form-control', 'id' => 'receiver', 'required']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Image', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::file('images', ['class'=>'form-control','id'=>'receiver']) !!}
+                                            {!! Form::file('images', ['class' => 'form-control', 'id' => 'receiver']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Content', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::textarea('content', null, ['class'=>'form-control','id'=>'summernote']) !!}
+                                            {!! Form::textarea('content', null, ['class' => 'form-control', 'id' => 'summernote']) !!}
                                         </div>
                                     </div>
                                     <div class="form-group row">
                                         <div class="col-sm-2">
                                             {!! Form::label('name', 'Background', ['class' => 'col-form-label']) !!}
-                                            <a href="" data-toggle="tooltip" data-placement="top" title="This image show in category bradcumb background">
+                                            <a href="" data-toggle="tooltip" data-placement="top"
+                                                title="This image show in category bradcumb background">
                                                 <i class="fa fa-info-circle"></i>
                                             </a>
                                         </div>
                                         <div class="col-sm-10">
-                                            {!! Form::file('background', ['class'=>'','id'=>'receiver']) !!}
-                                            <img src="{{ asset('public/images/') }}/{{ $page->background ?? ''  }}" width="100" class="img-fluid" alt="">
+                                            {!! Form::file('background', ['class' => '', 'id' => 'receiver']) !!}
+                                            <img src="{{ asset('public/images/') }}/{{ $page->background ?? '' }}"
+                                                width="100" class="img-fluid" alt="">
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Status', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::select('status', ['Active' => 'Active','Pending' => 'Pending'],null,['class'=>'form-control','id'=>'receiver','required']); !!}
+                                            {!! Form::select('status', ['Active' => 'Active', 'Pending' => 'Pending'], null, [
+                                                'class' => 'form-control',
+                                                'id' => 'receiver',
+                                                'required',
+                                            ]) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Meta Title', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('meta_title',null, ['class'=>'form-control','id'=>'receiver']) !!}
+                                            {!! Form::text('meta_title', null, ['class' => 'form-control', 'id' => 'receiver']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Meta Keyword', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('meta_keyword',null, ['class'=>'form-control','id'=>'receiver']) !!}
+                                            {!! Form::text('meta_keyword', null, ['class' => 'form-control', 'id' => 'receiver']) !!}
                                         </div>
                                     </div>
 
                                     <div class="form-group row">
                                         {!! Form::label('name', 'Meta Description', ['class' => 'col-sm-2 col-form-label']) !!}
                                         <div class="col-sm-10">
-                                            {!! Form::text('meta_description',null, ['class'=>'form-control','id'=>'receiver']) !!}
+                                            {!! Form::text('meta_description', null, ['class' => 'form-control', 'id' => 'receiver']) !!}
                                         </div>
                                     </div>
 
                                 </div>
                             </div>
+                            <div class="mt-3">
+                                <button type="submit" class="btn btn-info">Submit</button>
+                                <button onclick="window.history.back()" class="btn btn-default">Cancel</button>
+                            </div>
                         </div>
-                        <div class="card-footer">
-                            <button type="submit" class="btn btn-info">Submit</button>
-                            <button  onclick="window.history.back()" class="btn btn-default float-right">Cancel</button>
-                        </div>
+
                         {!! Form::close() !!}
                     </div>
                 </div>
@@ -110,9 +113,9 @@
     <!-- /.content -->
 @endsection
 @section('script')
-<script type="text/javascript">
-    $(document).ready(function() {
-      $('#summernote').summernote();
-    });
-</script>
+    <script type="text/javascript">
+        $(document).ready(function() {
+            $('#summernote').summernote();
+        });
+    </script>
 @endsection
--- resources/views/Admin/Pages/index.blade.php
+++ resources/views/Admin/Pages/index.blade.php
@@ -1,96 +1,103 @@
 @extends('Admin.layoutApp.app')
 
 @section('content')
-    <!-- Content Header (Page header) -->
-    <div class="content-header">
-        <div class="container-fluid">
-            <div class="row mb-2">
-                <div class="col-sm-6">
-                    <h1 class="m-0 text-dark">Page
-                        <a href="{{ route('madmin.pages.create') }}" class="btn btn-primary">Page Add</a>
-                    </h1>
-                </div><!-- /.col -->
-                <div class="col-sm-6">
-                    <ol class="breadcrumb float-sm-right">
-                        <li class="breadcrumb-item"><a href="#">Page</a></li>
-                        <li class="breadcrumb-item active">Page add</li>
-                    </ol>
-                </div><!-- /.col -->
-            </div><!-- /.row -->
-        </div><!-- /.container-fluid -->
-    </div>
-    <!-- /.content-header -->
 
-    <!-- Main content -->
+    @include('Admin.include.breadcrumb', [
+        'page' => __($type == 'about' ? 'About Us' : 'Page List'),
+        'parent' => __('Home'),
+        'child' => __($type == 'about' ? 'About' : 'Pages'),
+        'button' => __($type == 'about' ? 'Add About' : 'Add Page'),
+        'button_icon' => 'lni lni-plus',
+        'route' => $type == 'about' ? route('madmin.aboutadmin.create') : route('madmin.pages.create'),
+    ])
+
     <section class="content">
         <div class="container-fluid">
-            <!-- Small boxes (Stat box) -->
             <div class="row">
                 <div class="col-md-12">
                     <div class="card">
-                    {{--                        <div class="card-header">--}}
-                    {{--                            <h3 class="card-title">Condensed Full Width Table</h3>--}}
-                    {{--                        </div>--}}
-                    <!-- /.card-header -->
 
                         @include('Admin.include.message')
-                        <div class="card-body p-0">
-                            <table class="table table-bordered">
-                                <tbody>
-                                <tr>
-                                    <th>SI</th>
-                                    <th>Title</th>
-                                    <th>Slug</th>
-                                    <th>Thumb</th>
-                                    <th>Status</th>
-                                    <th>Date</th>
-                                    <th width="50">Action</th>
-                                </tr>
-                                @foreach($pages as $value)
-                                    <tr>
-                                        <td>{{ $value->id }}</td>
-                                        <td>{{ $value->title }}</td>
-                                        <td>
-                                            {{ $value->slug }}
-                                        </td>
-                                        <td>
-                                            @if($value->thumb==NULL)
-                                                <img src="{{ asset('public/asset/') }}/notfind.png" width="80" height="60">
+
+                        <div class="card-body">
+                            <div class="table-responsive">
+                                <table class="table table-bordered table-hover mb-0">
+                                    <thead class="custom-thead">
+                                        <tr>
+                                            @if($type == 'about')
+                                                <th>About Title</th>
+                                                <th>Content</th>
+                                                <th>Mission</th>
+                                                <th>Vision</th>
+                                                <th>Establishment</th>
+                                                <th>Date</th>
+                                                <th>Action</th>
                                             @else
-                                                <img src="{{ asset('public/images/pages/') }}/{{ $value->thumb }}" width="80" height="60">
+                                                <th>SI</th>
+                                                <th>Title</th>
+                                                <th>Slug</th>
+                                                <th class="d-none d-md-table-cell">Thumb</th>
+                                                <th>Status</th>
+                                                <th>Date</th>
+                                                <th>Action</th>
                                             @endif
-
-                                        </td>
-                                        <td>{{ $value->status }}</td>
-                                        <td>{{ $value->created_at->diffForHumans() }} </td>
-
-                                        <td>
-                                            <div class="row">
-                                                    <a href="{{route('madmin.pages.edit',$value->id)}}" class="btn btn-success m-1"><i class="lni-pencil-alt"></i> </a>
-{{--                                                    {!! Form::open(['method'=>'DELETE','route'=>['madmin.pages.destroy',$value->id]]) !!}--}}
-{{--                                                    <button type="submit" value="Delete" class="btn btn-danger m-1" onclick="return confirm('Do you want to Delete')"><i class="lni-trash"></i></button>--}}
-{{--                                                    {!! Form::close() !!}--}}
-
-                                            </div>
-                                        </td>
-                                    </tr>
-                                @endforeach
-
-                                </tbody>
-                            </table>
-
+                                        </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($data as $value)
+                                            <tr>
+                                                @if($type == 'about')
+                                                    <td>{{ $value->about_title }}</td>
+                                                    <td>{{ Str::limit(strip_tags($value->content), 60) }}</td>
+                                                    <td>{{ $value->mission }}</td>
+                                                    <td>{{ $value->vision }}</td>
+                                                    <td>{{ $value->establistmet }}</td>
+                                                    <td>{{ $value->created_at->diffForHumans() }}</td>
+                                                    <td>
+                                                        <div class="d-flex flex-wrap">
+                                                            <a href="{{ route('madmin.aboutadmin.edit', $value->id) }}" class="btn btn-success btn-sm m-1">
+                                                                <i class="lni-pencil-alt"></i>
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                @else
+                                                    <td>{{ $loop->iteration }}</td>
+                                                    <td>{{ $value->title }}</td>
+                                                    <td>{{ $value->slug }}</td>
+                                                    <td class="d-none d-md-table-cell">
+                                                        @if ($value->thumb == null)
+                                                            <img src="{{ asset('public/asset/notfind.png') }}" width="80" height="60" class="img-fluid">
+                                                        @else
+                                                            <img src="{{ asset('public/images/pages/' . $value->thumb) }}" width="80" height="60" class="img-fluid">
+                                                        @endif
+                                                    </td>
+                                                    <td>{{ $value->status }}</td>
+                                                    <td>{{ $value->created_at->diffForHumans() }}</td>
+                                                    <td>
+                                                        <div class="d-flex flex-wrap">
+                                                            <a href="{{ route('madmin.pages.edit', $value->id) }}" class="btn btn-success btn-sm m-1">
+                                                                <i class="lni-pencil-alt"></i>
+                                                            </a>
+                                                        </div>
+                                                    </td>
+                                                @endif
+                                            </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                            </div>
                         </div>
-                        <!-- /.card-body -->
-                    </div>
-                    <div class="row">
-                        <div class="col-md-6 offset-md-2 m-0 float-right">
-                            {{ $pages->render() }}
+
+                        <div class="card-footer clearfix">
+                            <div class="float-right">
+                                {{ $data->links() }}
+                            </div>
                         </div>
+
                     </div>
                 </div>
             </div>
-            <!-- /.row (main row) -->
-        </div><!-- /.container-fluid -->
+        </div>
     </section>
-    <!-- /.content -->
+
 @endsection
