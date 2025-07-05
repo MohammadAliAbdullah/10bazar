4d06fd1235727f6146c28f3443544088b7d65a65
ma_abdullah <maabdullah511@gmail.com>
2025-07-05 13:55:19 +0600

    bread resposive

--- resources/views/Admin/include/breadcrumb.blade.php
+++ resources/views/Admin/include/breadcrumb.blade.php
@@ -1,31 +1,37 @@
 <div class="container-fluid">
     <!-- Breadcrumb Start -->
-    <div class="breadcrumb-wrapper row">
-        <div class="col-12 col-lg-3 col-md-6">
-            <h4 class="page-title">{{ $page }}</h4>
+    <div class="d-flex flex-wrap justify-content-between align-items-center mb-3">
+        <div class="page-title mb-2 mb-sm-0">
+            <h4 class="mb-0">{{ $page }}</h4>
         </div>
-        <div class="col-12 col-lg-9 col-md-6">
-            <ol class="breadcrumb float-right">
+
+        <div class="breadcrumb-area">
+            <ol class="breadcrumb mb-0 bg-transparent">
                 @if (isset($multipleBtn) && $multipleBtn != '')
                     @foreach ($multipleBtn as $btn)
-                        <li><a href="{{ $btn['route'] }}" class="btn {{ $btn['class'] }} text-white">
-                                <i class="{{ $btn['icon'] }}"></i> &nbsp {{ $btn['name'] }} </a></li>
+                        <li class="breadcrumb-item">
+                            <a href="{{ $btn['route'] }}" class="btn {{ $btn['class'] }} btn-sm text-white">
+                                <i class="{{ $btn['icon'] }}"></i> {{ $btn['name'] }}
+                            </a>
+                        </li>
                     @endforeach
                 @else
                     @if (!empty($page) && $route != '#')
-                        <li><a href="{{ $route }}" class="btn btn-success text-white"><i
-                                    class="{{ isset($button_icon) ? $button_icon : '' }}"></i> &nbsp {{ isset($button) ? $button : '' }} </a></li>
+                        <li class="breadcrumb-item">
+                            <a href="{{ $route }}" class="btn btn-success btn-sm text-white">
+                                <i class="{{ $button_icon ?? '' }}"></i> {{ $button ?? '' }}
+                            </a>
+                        </li>
                     @elseif (!empty($parent) && $route == '#')
-                        <li><a href="javascript:void(0)">{{ $parent }}</a></li>
-                        <li class="active">/ {{ $page }}</li>
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">{{ $parent }}</a></li>
+                        <li class="breadcrumb-item active">{{ $page }}</li>
                     @elseif ($route == '#')
-                        <li><a href="javascript:void(0)">Home</a></li>
-                        <li class="active">/ {{ $child ?? $page }}</li>
+                        <li class="breadcrumb-item"><a href="javascript:void(0)">Home</a></li>
+                        <li class="breadcrumb-item active">{{ $child ?? $page }}</li>
                     @else
-                        <li class="active">/ {{ $page ?? 'Page' }}</li>
+                        <li class="breadcrumb-item active">{{ $page ?? 'Page' }}</li>
                     @endif
                 @endif
-
             </ol>
         </div>
     </div>
